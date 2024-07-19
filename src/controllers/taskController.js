89f94e70e61d0a0f04ceed71d9e39ef48b16a7ed
Sebastian Fonseca <sebastian.fonseca@soldife.com>
2024-07-19 11:05:34 -0500

    feature: add endpoint for edit task

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -22,8 +22,28 @@ const createTask = async (req, res) => {
         res.status(500).error(error.message);
     }
 }
+
+// Edit tasks
+const editTask = async (req, res) => {
+    const id = req.params.id;
+    const { title, description } = req.body;
+    try {
+        if(!title || !description ){
+            return res.status(400).json({ message: 'Todos los campos son obligatorios.' });
+        }
+        const taskUpdate = await Task.findOneAndUpdate({_id: id}, {title: title, description: description}, {new: true})
+        return res.status(200).json({
+            status: 'success',
+            message: 'Tarea actualizada',
+            task: taskUpdate
+        })
+    } catch (error) {
+        res.status(500).error(error.message);
+    }
+}
  
 export  {
     getTasks,
-    createTask
+    createTask,
+    editTask
 }
--- src/routes/tasksRoutes.js
+++ src/routes/tasksRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getTasks, createTask } from '../controllers/taskController.js';
+import { getTasks, createTask, editTask } from '../controllers/taskController.js';
 
 const router = express.Router();
 
@@ -8,5 +8,8 @@ router.get('/', getTasks);
 
 // Create a new task
 router.post('/', createTask);
+
+// Edit a task
+router.put('/:id', editTask)
 
 export default router;
